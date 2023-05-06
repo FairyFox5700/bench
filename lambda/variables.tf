634132b1aa77bd77e881e30fb2ddf19debd51119
Iryna Tyshchenko <iryna.tyshchenko@ml-pa.com>
2023-05-06 19:41:38 +0200

    var update

--- lambda/variables.tf
+++ lambda/variables.tf
@@ -1,14 +1,14 @@
 variable "bench_execution_result_backet_id" {
-  value       = var.bench_execution_results.id
   type        = string
+  description = "Id of the benchmark execution result backet"
 }
 
 variable "python_code_dep_backet_id" {
-  value       = var.python_code_dep.id
+  description = "Id of the python code dep backet"
   type        = string
 }
 
 variable "s3_bench_processing_results_id" {
-  value       = var.s3_bench_processing_results.id
+  description = "Id of the s3 benchmark processing results"
   type        = string
 }
