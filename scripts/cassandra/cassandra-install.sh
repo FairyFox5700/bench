4199aa35d5906ed5a23045678695008946cf29aa
Iryna Tyshchenko <iryna.tyshchenko@ml-pa.com>
2023-05-21 14:33:15 +0200

    cassandra install script update

--- scripts/cassandra/cassandra-install.sh
+++ scripts/cassandra/cassandra-install.sh
@@ -3,8 +3,8 @@ sudo apt-get update
 sudo apt install default-jdk -y
 sudo apt install wget
 wget -q -O - https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
-echo "deb http://www.apache.org/dist/cassandra/debian 40x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list deb http://www.apache.org/dist/cassandra/debian 40x main
+echo "deb https://debian.cassandra.apache.org 40x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
 sudo apt-get update
-sudo apt install cassandra -y
+sudo apt-get install cassandra -y
 sudo systemctl enable cassandra
 sudo systemctl start cassandra
