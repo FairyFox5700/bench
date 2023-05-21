c30eeb8bbd59065d9f077901569e33baec2da723
Iryna Tyshchenko <iryna.tyshchenko@ml-pa.com>
2023-05-21 23:32:57 +0200

    s3 name

--- scripts/copy-to-s3.sh
+++ scripts/copy-to-s3.sh
@@ -4,4 +4,4 @@ sudo apt-get install awscli -y
 # Set default region
 aws configure set region "eu-west-1" --profile default
 
-aws s3 cp /home/ubuntu/results s3://bench-execution-results-pipeline/ --recursive
+aws s3 cp /home/ubuntu/results s3://bench-execution-results-pipeline-bucket/ --recursive
